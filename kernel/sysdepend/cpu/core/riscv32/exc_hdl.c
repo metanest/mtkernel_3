3c94e5da23c8eafbe8938be9e0cce08009d589cd
KISHIMOTO, Makoto <ksmakoto@dd.iij4u.or.jp>
2023-02-28 20:34:58 +0900

    使ってない例外ハンドラのコードを消した

--- kernel/sysdepend/cpu/core/riscv32/exc_hdl.c
+++ kernel/sysdepend/cpu/core/riscv32/exc_hdl.c
@@ -25,43 +25,7 @@ WEAK_FUNC EXPORT void Exception_Handler(void)
 	EXCEPTION_DBG_MSG("Exception\n");
 	while(1);
 }
-#if 0
-/*
- * Undefined instruction exception handler
- */
-WEAK_FUNC EXPORT void UndefinedInst_Handler(void)
-{
-	EXCEPTION_DBG_MSG("Undef\n");
-	while(1);
-}
 
-/*
- * Prefetch abort exception handler
- */
-WEAK_FUNC EXPORT void PrefetchAbort_Handler(void)
-{
-	EXCEPTION_DBG_MSG("iabort\n");
-	while(1);
-}
-
-/*
- * Data abort exception handler
- */
-WEAK_FUNC EXPORT void DataAbort_Handler(void)
-{
-	EXCEPTION_DBG_MSG("dabort\n");
-	while(1);
-}
-
-/*
- * VFP invalid handler
- */
-WEAK_FUNC EXPORT void VFPInvalid_Handler(void)
-{
-	EXCEPTION_DBG_MSG("VFP invalid\n");
-	while(1);
-}
-#endif
 /*
  * Default Handler (Undefine Interrupt)
  */
@@ -70,14 +34,5 @@ EXPORT void Default_Handler(void)
 	EXCEPTION_DBG_MSG("Undefine Interrupt\n");
 	while(1);
 }
-#if 0
-/*
- * SVC default Handler (Undefine SVC)
- */
-WEAK_FUNC EXPORT void SVC_default_Handler(void)
-{
-	EXCEPTION_DBG_MSG("Undefine SVC\n");
-	while(1);
-}
-#endif
+
 #endif	/* CPU_CORE_RISCV32 */
